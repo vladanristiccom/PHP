c884bad62d46ebcbda5a5de89f6574aa8fd4c568
vladanristiccom <65029662+vladanristiccom@users.noreply.github.com>
2020-05-08 17:05:17 +0200

    Add files via upload

---
+++ 1. Uvod/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php 
+
+		echo "Hello World!";
+
+	?>
+
+	<h1>Test</h1>
+
+	<?php 
+
+		echo "Hello PHP";
+				
+	?>
+
+</body>
+</html>
+
+
